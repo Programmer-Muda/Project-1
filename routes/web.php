c286b50d18b78414fdc01af6aa2aa9d533858ab6
Ahmad Reza <hungry.dev39@gmail.com>
2018-12-09 14:18:35 +0700

    Rutenya login & register selesai

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function() {
+    return view('login');
+});
+
+Route::get('/daftar', function(){
+    return view('register');
+})
