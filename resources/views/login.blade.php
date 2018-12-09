b773d8656ff48c2088a6e4bfb2ef7fdd3b6bcd50
Reyhan <rjeck64@gmail.com>
2018-12-09 19:31:41 +0700

    Login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+<form>
+  <div class="form-group">
+    <label for="email1">Email address</label>
+    <input type="email" class="form-control" id="email1" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="Password1">Password</label>
+    <input type="password" class="form-control" id="Password1" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</body>
+</html>
