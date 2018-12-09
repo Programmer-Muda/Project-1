056803fb1a1836617d07403a070f0faa2a05cabe
Reyhan <rjeck64@gmail.com>
2018-12-09 19:53:35 +0700

    css nya

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,9 +2,12 @@
 <html>
 <head>
   <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
+  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
 </head>
 <body>
 <form>
+<div class="tabel">
   <div class="form-group">
     <label for="email1">Email address</label>
     <input type="email" class="form-control" id="email1" placeholder="Enter email">
@@ -14,6 +17,7 @@
     <input type="password" class="form-control" id="Password1" placeholder="Password">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
+</div>
 </form>
 </body>
 </html>
