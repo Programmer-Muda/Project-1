0d6bfcce4f3919d0504cf5b526d8970086c038fd
Ahmad Reza <hungry.dev39@gmail.com>
2018-12-10 22:37:20 +0700

    percantik halaman depan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
         /* Padding below the footer and lighter body text */
 
         body {
-          padding-top: 3rem;
+          padding-top : 0;
           padding-bottom: 3rem;
           color: #5a5a5a;
         }
@@ -107,74 +107,28 @@
   <body>
 
     <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <nav class="navbar navbar-expand-md navigasi">
         <a class="navbar-brand" href="#">Project-01</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-            </ul>
-          <ul class="navbar-nav mt-2 mt-md-0">
-            <li class="nav-item">
-              <a class="nav-link" href="/register">Daftar</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/login">Masuk</a>
-            </li>
-        </ul>
+          <div class="navbar-nav mt-2 mt-md-0 ml-auto">
+            <div class="btn-group">
+              <a class="btn btn-primary " id="khusus">Login</a>
+              <a class="btn btn-primary ">Register</a>
+            </div>
+          </div>
         </div>
       </nav>
     </header>
 
     <main role="main">
 
-      <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
-            <div class="container">
-              <div class="carousel-caption text-left">
-                <h1>Belajar Bersama</h1>
-                <p>Mari mulai belajar bersama kita, lalu mulai daftar untuk memulai</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">daftar</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
-            <div class="container">
-              <div class="carousel-caption">
-                <h1>Belajar Lagi</h1>
-                <p>Pelajari berbagai macam rumus untuk memecahkan soal soal yang sedang kamu pelajari </p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Rumus</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
-            <div class="container">
-              <div class="carousel-caption text-right">
-                <h1>Saling Bertanya Dan Saling Membantu sesama pelajar</h1>
-                <p>Mulailah dengan bertanya atau menjawab soal matematika yang sedang kamu pelajari</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Forum</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+      <div class="jumbotron jumbotron-fluid slet">
+        <h1>
+          Make the <strong>World</strong> <br> Be a <strong>Better</strong> place
+        </h1>
       </div>
 
 
