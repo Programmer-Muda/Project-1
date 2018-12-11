b01fc0f0744682663a6843d499ff66e0bf93d69d
Reyhan <rjeck64@gmail.com>
2018-12-11 19:30:40 +0700

    remake Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,28 +6,25 @@
   <!-- Sebelumnya Jangan lupa di cmd bikin npm run watch ya -->
 </head>
 <body>
-<center>
-<form>
-<div class="topbar">Login</div>
-<div class="tabel">
-  <div class="form-group">
-    <label for="email1">Email address</label>
-    <input type="email" class="form-control" id="email1" placeholder="Enter email" required>
-  </div>
-  <div class="form-group">
-    <label for="Password1">Password</label>
-    <input type="password" class="form-control" id="Password1" placeholder="Password" required>
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="Check" required>
-    <label class="form-check-label" for="Check">Check me</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+<div class="img">
+  <img src="{{ asset('img/blogin.jpg')}}" alt="">
 </div>
-</form>
-<div class="registered">
-<p>Not registered? <a href="/register">sign up now</a></p>
+<div class="sidebar">
+  <h5 class="topbar">LOGIN</h5>
+  <form action="">
+    <label>Email Address</label>
+    <input class="form-control" type="text" placeholder="Email..">
+    <label>Password</label>
+    <input class="form-control" type="password" placeholder="Password..">
+    <div class="custom-control custom-checkbox check">
+      <input type="check" class="custom-control-input" id="Check" required>
+      <label class="custom-control-label" for="Check">Check This</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Login Now</button>
+  </form>
+  <div class="sosmed">
+    <h5>Login With :</h5>
+  </div>
 </div>
-</center>
 </body>
 </html>
